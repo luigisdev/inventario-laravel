ad2cfca7ea92f987f647c976d43d784632351bc5
Luis García <luis_grdz@hotmail.com>
2019-08-12 00:01:23 -0500

    Video 4: Ruta y vista de la pagina principal

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Products</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Listado de productos
+                        <a href="" class="btn btn-success bn-sm float-right">Nuevo producto</a>
+                    </div>
+                    <div class="body">
+                        ...
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js" ></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,14 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|   
+    @Author: Luis Alberto García Rodríguez
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('products', function (){
+    return view('products.index');
 });
