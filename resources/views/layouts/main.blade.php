49d7ba92866e6ac00bd472cdca9490cdbe6bca9f
Luis García <luis_grdz@hotmail.com>
2019-08-15 22:15:43 -0500

    Video 15: vistas refactorizadas para utilizar una plantilla principal e inyectar el código de las distintas vistas

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
+
+    <title>Products</title>
+</head>
+<body>
+    @yield('contenido')
+
+    <script src="{{ url('js/jquery-3.4.1.min.js') }} "></script>
+    <script src="{{ url('js/bootstrap.min.js') }}" ></script>
+</body>
+</html>
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
-
-    <title>Products</title>
-</head>
-<body>
+@extends('layouts.main')
+@section('contenido')
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -36,8 +26,4 @@
             </div>
         </div>
     </div>
-
-    <script src="{{ url('js/jquery-3.4.1.min.js') }} "></script>
-    <script src="{{ url('js/bootstrap.min.js') }}" ></script>
-</body>
-</html>
+@endsection
